667565993f17de566e718fa38c7336c2b7bd4ab8
Manu974 <emmanuel.dijoux14@gmail.com>
2017-07-01 16:12:56 +0200

    modification des routes

--- js/supprimerUtilisateur.js
+++ js/supprimerUtilisateur.js
@@ -2,7 +2,7 @@
 $('ul').on("click", 'button[name="supprimerUtilisateur"]', function(){
 
   $.ajax({
-    url:'http://127.0.0.1:8000/users/'+this.id, 
+    url:'http://127.0.0.1:8000/api/users/'+this.id, 
     type : 'DELETE',
     timeout : 3000,
     success: function(data){
